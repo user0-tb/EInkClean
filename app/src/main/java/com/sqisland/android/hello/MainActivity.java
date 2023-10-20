8b930af82cc20d0b37d01fd57cdc96a15cf047ed
Test1524473 <104380885+user0-tb@users.noreply.github.com>
2023-10-20 14:33:05 +0200

    Update MainActivity.java

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_main);
   }
 }
