7edfeecacaac7cf9b3f0510dcd24786be044f566
Test1524473 <104380885+user0-tb@users.noreply.github.com>
2023-10-20 14:29:40 +0200

    Update MainActivity.java

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    getSupportActionBar().hide();
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE)
     setContentView(R.layout.activity_main);
   }
 }
